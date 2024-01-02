6de611a9a75872266123e068ebe48f3d8c4d0b8a
AmoNad24 <107304880+AmoNad24@users.noreply.github.com>
2024-01-02 11:40:10 +0530

    Update server.py

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, My Name is AmoghVinayNadkarni_1BM20IS017"
+    return "Hello, My Name is Boddula Vignesh 1BM20IS026"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
