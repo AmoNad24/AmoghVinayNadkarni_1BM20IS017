920db530de5304cafd1948426653af0d3031948a
AmoNad24 <107304880+AmoNad24@users.noreply.github.com>
2024-01-02 10:52:07 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,6 +1,10 @@
-FROM python:3.8-slim
-RUN mkdir /server
-ADD . /server
-WORKDIR /server
-RUN pip install -r requirements.txt
-CMD ["python", "server.py"]
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello, My Name is AmoghVinayNadkarni_1BM20IS017"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
